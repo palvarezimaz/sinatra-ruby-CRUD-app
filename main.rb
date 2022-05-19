da01736f995e1a333d8bf6f8cce5c9cc2ed9f56e
Ikilzig <palvarezimaz@gmail.com>
2022-05-19 11:41:47 +1000

    big update from older project

--- main.rb
+++ main.rb
@@ -1,11 +1,21 @@
-     
+
 require 'sinatra'
+require 'pry'
+require 'bcrypt'
 
-get '/' do
-  erb :index
-end
+enable :sessions
+
+require './controllers/crypto_controller'
+require './controllers/users_controller'
+require './controllers/session_controller'
+
+require './helpers/sessions_helper'
+
+require './models/crypto'
+require './models/user'
+
+require './db/db'
 
 
 
-
-
+  
