5a7fa9cf18e315e49b6e5daa8dcb13e5c46f0db2
Ikilzig <palvarezimaz@gmail.com>
2022-05-19 14:18:43 +1000

    users fix1

--- controllers/users_controller.rb
+++ controllers/users_controller.rb
@@ -2,7 +2,7 @@ get '/sign_up' do
   erb :'users/new'
 end
 
-post 'users/create_user' do
+post '/users/create_user' do
   name = params['name']
   email = params['email']
   password = params['password']
