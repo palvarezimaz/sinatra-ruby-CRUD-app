3cb9a7087e82edcde83cdcf031f71e0ac32fa8af
Ikilzig <palvarezimaz@gmail.com>
2022-05-19 13:06:53 +1000

    table updated

--- db/schema.sql
+++ db/schema.sql
@@ -2,7 +2,7 @@ CREATE DATABASE crud_app;
 
 \c crud_app
 
-CREATE TABLE elements(
+CREATE TABLE cryptos(
   id SERIAL PRIMARY KEY,
   name TEXT,
   acronym TEXT,
@@ -23,4 +23,4 @@ CREATE TABLE users(
   user_role TEXT
 );
 
-INSERT INTO elements(name, acronym, logo_url, year_created, year_ended, country, project_type, extra_info) VALUES('OneCoin', 'ONE', 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fyt3.ggpht.com%2Fa%2FAATXAJypkxrXl286sa3uFGKWLuOdA0xQHe5K3rx3Dw%3Ds900-c-k-c0xffffffff-no-rj-mo&f=1&nofb=1', 2014, 2017, 'Bulgaria', 'Ponzi Scam', 'OneCoin was one of the early crypto frauds. Its founder, the self-named CryptoQueen, Ruja Ignatova, hosted glitzy events across the world, including one in the U.K. Wembley Arena. There, she touted OneCoin as a Bitcoin Killer. Millions of investors would later end up defrauded in what turned out to be a $4 billion Ponzi scheme that used money from new investors to pay returns to the existing ones. Ignatova disappeared in 2017 when the net was finally closing in and police had filed a warrant for her arrest.');
+INSERT INTO cryptos(name, acronym, logo_url, year_created, year_ended, country, project_type, extra_info) VALUES('OneCoin', 'ONE', 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fyt3.ggpht.com%2Fa%2FAATXAJypkxrXl286sa3uFGKWLuOdA0xQHe5K3rx3Dw%3Ds900-c-k-c0xffffffff-no-rj-mo&f=1&nofb=1', 2014, 2017, 'Bulgaria', 'Ponzi Scam', 'OneCoin was one of the early crypto frauds. Its founder, the self-named CryptoQueen, Ruja Ignatova, hosted glitzy events across the world, including one in the U.K. Wembley Arena. There, she touted OneCoin as a Bitcoin Killer. Millions of investors would later end up defrauded in what turned out to be a $4 billion Ponzi scheme that used money from new investors to pay returns to the existing ones. Ignatova disappeared in 2017 when the net was finally closing in and police had filed a warrant for her arrest.');
